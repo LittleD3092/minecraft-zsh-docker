0da3a1ecc5f1c6be238b4fa53a67719f0d60ef85
littled3092 <drevild01801@gmail.com>
2024-08-05 15:23:43 +0800

    Add additional ram allocated to java

--- scripts/start-vanilla-server.sh
+++ scripts/start-vanilla-server.sh
@@ -7,4 +7,4 @@ cd ~/server
 SERVER_JAR=$(ls minecraft_server.*.jar | head -n 1)
 
 # Run the Minecraft server
-exec java -Xmx2048M -Xms2048M -jar $SERVER_JAR nogui
+exec java -Xmx8G -Xms2G -jar $SERVER_JAR nogui
